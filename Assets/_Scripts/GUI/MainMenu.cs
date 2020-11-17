584cf1ce52fdedc79471fbc001c32327f8f6a1e8
Terry Paton <terrypaton1@gmail.com>
2020-11-17 21:41:09 +1100

    Removing google play/achievements etc
    This functionality won’t be included in the open source version.

--- Assets/_Scripts/GUI/MainMenu.cs
+++ Assets/_Scripts/GUI/MainMenu.cs
@@ -2,9 +2,6 @@
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField]
-    protected GameObject donateButton;
-
     protected void OnEnable()
     {
         Messenger.AddListener(GlobalEvents.PurchaseMade, EvaluateDisplay);
@@ -18,40 +15,6 @@ public class MainMenu : MonoBehaviour
 
     private void EvaluateDisplay()
     {
-//		Debug.Log("showing main menu");
-        // has the player already donated to the developer?
-        var hasPlayerDonated = PlayerPrefs.GetInt(DataVariables.hasPlayerAlreadyDonated);
-//		hasPlayerDonated =0;
-//		Debug.Log("hasPlayerDonated:"+hasPlayerDonated);
-        if (hasPlayerDonated > 0)
-        {
-            // show thank you for supporting the developer
-            donateButton.SetActive(false);
-        }
-        else
-        {
-            var maxLevelBeaten = PlayerPrefs.GetInt(DataVariables.maxLevelBeatenPrefix);
-//			maxLevelBeaten = 5;
-//			// has the game been played more than 5 times?
-//			Debug.Log("maxLevelBeaten:"+maxLevelBeaten);
-            if (maxLevelBeaten >= 10)
-            {
-                // only show the donate button 40% of the time
-                if (Random.Range(0, 10) < 4)
-                {
-//					Debug.Log("Show donate button");
-// COMMENTING OUT THE DONATE BUTTON
-                    //donateButton.SetActive(true);
-                }
-                else
-                {
-                    donateButton.SetActive(false);
-                }
-            }
-            else
-            {
-                donateButton.SetActive(false);
-            }
-        }
+        var maxLevelBeaten = PlayerPrefs.GetInt(DataVariables.maxLevelBeatenPrefix);
     }
 }
--- Assets/_Scripts/Utils/CurrentBundleVersion.cs
+++ Assets/_Scripts/Utils/CurrentBundleVersion.cs
@@ -1,5 +1,5 @@
 public static class CurrentBundleVersion
 {
-	public static readonly string version = "1.0";
+	public static readonly string version = "1.15";
 }
 
