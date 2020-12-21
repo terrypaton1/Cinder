e934112e0bf9567e2f20726a4d9bc5f11ec526f3
Terry Paton <terrypaton1@gmail.com>
2020-12-21 11:25:57 +1100

    dding assets back into project 6

---
+++ Cinder/Assets/VisualAssets/Textures/RotateObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    [SerializeField]
+    protected float rotationSpeed = 5.0f;
+
+    protected void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
