5787804f8e117226da599a491d70e87252e47d1c
Terry Paton <terrypaton1@gmail.com>
2021-01-12 10:13:46 +1100

    Further increase TNT explosion range

--- Cinder/Assets/Scripts/Game/Bricks/TNTBrick.cs
+++ Cinder/Assets/Scripts/Game/Bricks/TNTBrick.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class TNTBrick : BrickBase
 {
-    private float explosionRange = 4;
+    private float explosionRange = 8;
 
     protected override IEnumerator DestroyBrickSequence(bool playSound = true)
     {
