2389b817ccf1118584e6cd9315f7ec00b38e3475
Terry Paton <terrypaton1@gmail.com>
2021-09-30 22:29:27 +1000

    Fix background display bug at highest levels

--- Cinder/Assets/Scripts/Game/Managers/Backgrounds.cs
+++ Cinder/Assets/Scripts/Game/Managers/Backgrounds.cs
@@ -11,7 +11,7 @@ public class Backgrounds : MonoBehaviour
     public void DisplayForLevel(int level)
     {
         var backgroundId = Mathf.FloorToInt(level / 5.0f);
-        backgroundId = Mathf.Clamp(backgroundId, 0, backgrounds.Length);
+        backgroundId = Mathf.Clamp(backgroundId, 0, backgrounds.Length - 1);
 
         var sprite = backgrounds[backgroundId];
         background.sprite = sprite;
